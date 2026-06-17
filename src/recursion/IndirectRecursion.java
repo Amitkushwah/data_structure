c3db774e537b5b3f20c323378806841a532ce6a1
Amit Kushwah <kushwah2506@yahoo.com>
2026-06-18 00:00:57 +0530

    Add implementations of factorial, indirect, nested, and sum of N no. recursion algorithms

---
+++ src/recursion/IndirectRecursion.java
@@ -0,0 +1,20 @@
+package recursion;
+
+public class IndirectRecursion {
+    static void funA(int n){
+        if(n>0){
+            System.out.println(n);
+            funB(n-1);
+        }
+    }
+    static void funB(int n){
+        if(n>1){
+            System.out.println(n);
+            funA(n/2);
+        }
+    }
+
+    public static void main(String[] args) {
+        funA(20);
+    }
+}
---
+++ src/recursion/SumOfNWithAB.java
@@ -0,0 +1,13 @@
+package recursion;
+
+public class SumOfNWithAB {
+    static int sum(int n){
+        if (n == 0)
+            return 0;
+        return sum(n-1)+n;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(10));
+    }
+}
