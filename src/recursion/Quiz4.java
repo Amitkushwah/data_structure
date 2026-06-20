756f668a54b7f90c0d74e2a1d3fde9207eb4c2f7
Amit Kushwah <kushwah2506@yahoo.com>
2026-06-20 15:41:28 +0530

    Add recursive function implementation in Quiz4.java

---
+++ src/recursion/Quiz4.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class Quiz4 {
+    static int fun (int n)
+    {
+        int x=1, k;
+        if (n==1) return x;
+        for (k=1; k<n; ++k)
+            x=x + fun (k) * fun (n - k);
+        return x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fun(5));
+    }
+}
