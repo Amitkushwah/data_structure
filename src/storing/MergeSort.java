314de0d27fc2b6950942a86891eac85fbc658dc3
Amit Kushwah <kushwah2506@yahoo.com>
2026-06-23 23:29:34 +0530

    Add MergeSort.java for implementing merge sort algorithm

---
+++ src/storing/MergeSort.java
@@ -0,0 +1,50 @@
+package storing;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeSort {
+    static void divide(int[] arr, int low, int high){
+        if(high == low)
+            return;
+        int mid = (high+low)/2;
+        divide(arr, low, mid);
+        divide(arr, mid+1, high);
+        merge(arr, low, mid, high);
+    }
+    static void merge(int[] arr, int low, int mid, int high){
+        List<Integer> temp = new ArrayList<>();
+        int left = low;
+        int right = mid+1;
+        while(left <= mid && right <= high){
+            if(arr[left] <= arr[right]){
+                temp.add(arr[left]);
+                left++;
+            }
+            else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+        while (left <= mid){
+            temp.add(arr[left]);
+            left++;
+        }
+        while (right <= high){
+            temp.add(arr[right]);
+            right++;
+        }
+        for(int i = 0; i <= high; i++)
+            arr[i] = temp.get(i);
+    }
+    static void mergeSort(int[] arr){
+        divide(arr, 0, arr.length-1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,65,7,34,67,9,8,12,3};
+        mergeSort(arr);
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+}
