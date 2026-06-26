7019c3937aae16e9c51a2476757557bb7bd1bcc8
Amit Kushwah <kushwah2506@yahoo.com>
2026-06-26 23:33:00 +0530

    Add QuickSort.java for implementing quick sort algorithm

---
+++ src/storing/QuickSort.java
@@ -0,0 +1,42 @@
+package storing;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    static void quickSort(int[] arr, int low, int high){
+        if(low < high){
+            int pIndex = partition(arr, low, high);
+            quickSort(arr, low, pIndex-1);
+            quickSort(arr, pIndex+1, high);
+        }
+    }
+    static int partition(int[] arr, int low, int high){
+        int pivot = arr[low];
+        int i = low;
+        int j = high;
+
+        while(i<j){
+            while(arr[i] <= pivot && i <= high - 1){
+                i++;
+            }
+            while (arr[j] > pivot && j <= low + 1){
+                j--;
+            }
+            if(i < j){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[low];
+        arr[low] = arr[j];
+        arr[j] = temp;
+        return j;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,5,3,7,4,5,1,9};
+        quickSort(arr, 0, arr.length-1);
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+}
