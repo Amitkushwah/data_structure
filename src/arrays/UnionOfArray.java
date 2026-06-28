07641e78d46a788f06c9c509f11e9a16920b5d49
Amit Kushwah <kushwah2506@yahoo.com>
2026-06-28 23:03:00 +0530

    Add UnionOfArray.java to implement union of two arrays

---
+++ src/arrays/UnionOfArray.java
@@ -0,0 +1,46 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class UnionOfArray {
+    static List<Integer> union(int[] a, int[] b){
+        int n1 = a.length;
+        int n2 = b.length;
+        int i = 0;
+        int j = 0;
+        List<Integer> result = new ArrayList<>();
+
+        while (i < n1 && j < n2){
+            if(a[i] <= b[j]){
+                if(result.isEmpty() || result.get(result.size() - 1) != a[i] )
+                    result.add(a[i]);
+                i++;
+            }
+            else {
+                if(result.isEmpty() || result.get(result.size() - 1) != b[j])
+                    result.add(b[j]);
+                j++;
+            }
+        }
+        while (i < n1){
+            if(result.isEmpty() || result.get(result.size() - 1) != a[i])
+                result.add(a[i]);
+            i++;
+        }
+        while (j < n2){
+            if(result.isEmpty() || result.get(result.size() - 1) != b[j])
+                result.add(b[j]);
+            j++;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2,4,5,1,6,4};
+        int[] b = {4,6,2,4,9,3};
+        List<Integer> l = union(a, b);
+        l.stream().forEach(System.out::println);
+    }
+}
