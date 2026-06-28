78a8ce3a463401ab3dda74b127432df5b2c18126
Amit Kushwah <kushwah2506@yahoo.com>
2026-06-28 22:04:57 +0530

    Refactor moveZero and moveZeroOptimal methods in MoveZeroToLast.java for improved readability and performance

--- src/arrays/MoveZeroToLast.java
+++ src/arrays/MoveZeroToLast.java
@@ -5,27 +5,30 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MoveZeroToLast {
-    static void moveZero(int[] arr){
+    static void moveZero(int[] arr) {
         int n = arr.length;
         List<Integer> temp = new ArrayList<>();
-        for(int i = 0; i < n; i++)
-            if(arr[i] != 0)
+        for (int i = 0; i < n; i++)
+            if (arr[i] != 0)
                 temp.add(arr[i]);
-        for(int i = 0; i < temp.size(); i++)
+        for (int i = 0; i < temp.size(); i++)
             arr[i] = temp.get(i);
-        for(int i = temp.size(); i < n; i++)
+        for (int i = temp.size(); i < n; i++)
             arr[i] = 0;
     }
-    static void moveZeroOptimal(int[] arr){
+
+    static void moveZeroOptimal(int[] arr) {
         int j = -1;
         int n = arr.length;
-        for(int i = 0; i < n; i++)
-            if(arr[i] == 0) {
+        for (int i = 0; i < n; i++)
+            if (arr[i] == 0) {
                 j = i;
                 break;
             }
-        for(int i = j+1; i < n; i++)
-            if(arr[i] != 0){
+        if (j == -1)
+            return;
+        for (int i = j + 1; i < n; i++)
+            if (arr[i] != 0) {
                 int temp = arr[i];
                 arr[i] = arr[j];
                 arr[j] = temp;
@@ -34,7 +37,7 @@ public class MoveZeroToLast {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,0,5,6,0,0,3,0,2};
+        int[] arr = {1, 0, 5, 6, 0, 0, 3, 0, 2};
 //        moveZero(arr);
         moveZeroOptimal(arr);
         Arrays.stream(arr).forEach(System.out::println);
