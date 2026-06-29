bb98565178e5febc4727de03a9bb2be4886f7d20
Amit Kushwah <kushwah2506@yahoo.com>
2026-06-29 22:12:57 +0530

    Add intersectionOptimal method in IntersectionOfArray.java for efficient intersection calculation

--- src/arrays/IntersectionOfArray.java
+++ src/arrays/IntersectionOfArray.java
@@ -24,10 +24,31 @@ public class IntersectionOfArray {
         return ans;
     }
 
+    static List<Integer> intersectionOptimal(int[] arr1, int[] arr2) {
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+        List<Integer> ans = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while (i < n1 && j < n2) {
+            if (arr1[i] < arr2[j]) {
+                i++;
+            } else if (arr2[j] < arr1[i]) {
+                j++;
+            } else {
+                ans.add(arr1[i]);
+                i++;
+                j++;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {1, 2, 3, 5, 7, 8};
-        List<Integer> ans = intersection(arr1, arr2);
+//        List<Integer> ans = intersection(arr1, arr2);
+        List<Integer> ans = intersectionOptimal(arr1, arr2);
         ans.forEach(System.out::println);
 
     }
