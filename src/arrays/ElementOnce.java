c407b607bce3ef6a35d3adcf80f5e56ddbab34a0
Amit Kushwah <kushwah2506@yahoo.com>
2026-06-30 23:52:41 +0530

    Add findElementAppearsOnceBetter method in ElementOnce.java for improved element search

--- src/arrays/ElementOnce.java
+++ src/arrays/ElementOnce.java
@@ -15,8 +15,28 @@ public class ElementOnce {
         return -1;
     }
 
+    static int findElementAppearsOnceBetter(int[] arr) {
+        int n = arr.length;
+        int maxi = 0;
+        for (int i : arr) {
+            maxi = Math.max(maxi, i);
+        }
+        int[] hash = new int[maxi + 1];
+        for (int i = 0; i < n; i++) {
+            hash[arr[i]]++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (hash[arr[i]] == 1)
+                return arr[i];
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 1, 2, 3, 3, 4, 4};
         System.out.println(findElementAppearsOnce(arr));
+        System.out.println(findElementAppearsOnceBetter(arr));
+
     }
 }
