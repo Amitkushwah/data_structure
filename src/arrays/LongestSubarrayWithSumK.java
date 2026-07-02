279487df9c897f383000a2d1f102da457a35c5fd
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-02 22:41:43 +0530

    Refactor longestSubarrayOptimal method in LongestSubarrayWithSumK.java for improved readability and formatting

--- src/arrays/LongestSubarrayWithSumK.java
+++ src/arrays/LongestSubarrayWithSumK.java
@@ -43,22 +43,22 @@ public class LongestSubarrayWithSumK {
         return maxLen;
     }
 
-    static int longestSubarrayOptimal(int[] arr, int k){ // Optimal if array contains only zeros and positive elements
+    static int longestSubarrayOptimal(int[] arr, int k) { // Optimal if array contains only zeros and positive elements
         int n = arr.length;
         int right = 0, left = 0;
         int sum = arr[0];
         int maxLen = 0;
 
-        while(right < n){
-            while (left <= right && sum > k){
+        while (right < n) {
+            while (left <= right && sum > k) {
                 sum -= arr[left];
                 left++;
             }
-            if(sum == k){
+            if (sum == k) {
                 maxLen = Math.max(maxLen, right - left + 1);
             }
             right++;
-            if(right < n){
+            if (right < n) {
                 sum += arr[right];
             }
         }
