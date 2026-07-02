f8d843ae5dc05a7d40218751c42e583b40c13e67
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-02 22:02:23 +0530

    Add longestSubarrayBetter method in LongestSubarrayWithSumK.java for improved subarray sum calculation

--- src/arrays/LongestSubarrayWithSumK.java
+++ src/arrays/LongestSubarrayWithSumK.java
@@ -1,5 +1,8 @@
 package arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubarrayWithSumK {
     static int longestSubarray(int[] arr, int k) {
         int n = arr.length;
@@ -16,9 +19,33 @@ public class LongestSubarrayWithSumK {
         return maxLen;
     }
 
+    static int longestSubarrayBetter(int[] arr, int k) {
+        int n = arr.length;
+        int sum = 0;
+        int maxLen = 0;
+        Map<Integer, Integer> preSumMap = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            if (sum == k)
+                maxLen = Math.max(maxLen, i + 1);
+
+            int rem = sum - k;
+            if (preSumMap.containsKey(rem)) {
+                int len = i - preSumMap.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            if (!preSumMap.containsKey(sum))
+                preSumMap.put(sum, i);
+
+        }
+        return maxLen;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 1, 1, 1, 1, 4, 2, 3};
-        int target = 4;
-        System.out.println(longestSubarray(arr, target));
+        int target = 3;
+        System.out.println(longestSubarrayBetter(arr, target));
     }
 }
