faa36a188212958c6fbb8bcb6fb5e02a4cf7788b
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-02 23:03:02 +0530

    Refactor while loop formatting in longestSubarrayOptimal method for improved readability

--- src/arrays/LongestSubarrayWithSumK.java
+++ src/arrays/LongestSubarrayWithSumK.java
@@ -49,17 +49,17 @@ public class LongestSubarrayWithSumK {
         int sum = arr[0];
         int maxLen = 0;
 
-        while(right < n){
-            while (left <= right && sum > k){
+        while (right < n) {
+            while (left <= right && sum > k) {
                 sum -= arr[left];
                 left++;
             }
 
             if (sum == k)
-                maxLen = Math.max(maxLen, right - left +1);
+                maxLen = Math.max(maxLen, right - left + 1);
 
             right++;
-            if(right < n)
+            if (right < n)
                 sum += arr[right];
         }
         return maxLen;
