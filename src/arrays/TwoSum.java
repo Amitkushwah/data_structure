b806ab2499418bfc85b7b54565459a0503a03a3f
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-05 12:07:20 +0530

    Add twoSumBetter method for optimized two-sum solution

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -13,10 +13,25 @@ public class TwoSum {
         return new int[]{-1, -1};
     }
 
+    static int[] twoSumBetter(int[] arr, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int a = arr[i];
+            int more = target - a;
+            if (map.containsKey(more)) {
+                return new int[]{i, map.get(more)};
+            }
+            map.put(a, i);
+        }
+        return new int[]{-1, -1};
+    }
+
     public static void main(String[] args) {
         int[] arr = {2,6,5,8,11};
         int target = 14;
         int[] ans = twoSum(arr, target);
         System.out.println(ans[0]+" "+ans[1]);
+        int[] ans2 = twoSumBetter(arr, target);
+        System.out.println(ans2[0]+" "+ans2[1]);
     }
 }
