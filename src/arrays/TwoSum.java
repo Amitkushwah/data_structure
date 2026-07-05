ac5710878adf71d1360b5a5d04046a9447da5ce4
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-05 14:08:47 +0530

    Add twoSumOptimal method for efficient two-sum check using two-pointer technique

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -26,12 +26,28 @@ public class TwoSum {
         return new int[]{-1, -1};
     }
 
+    static boolean twoSumOptimal(int[] arr, int target) {
+        int n = arr.length;
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == target)
+                return true;
+            else if (sum < target)
+                left++;
+            else
+                right--;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        int[] arr = {2,6,5,8,11};
+        int[] arr = {2, 6, 5, 8, 11};
         int target = 14;
         int[] ans = twoSum(arr, target);
-        System.out.println(ans[0]+" "+ans[1]);
+        System.out.println(ans[0] + " " + ans[1]);
         int[] ans2 = twoSumBetter(arr, target);
-        System.out.println(ans2[0]+" "+ans2[1]);
+        System.out.println(ans2[0] + " " + ans2[1]);
+        System.out.println(twoSumOptimal(arr, target));
     }
 }
