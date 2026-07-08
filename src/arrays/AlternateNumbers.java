18277e1c700ea0e13d51b5b9d231f569a2065118
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-08 22:25:51 +0530

    Refactor AlternateNumbers class to include alternateNoBetter method for improved rearrangement of array elements

--- src/arrays/AlternateNumbers.java
+++ src/arrays/AlternateNumbers.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class AlternateNumbers {
-    static int[] alternateNo(int[] arr) {
+    static void alternateNo(int[] arr) {
         List<Integer> pos = new ArrayList<>();
         List<Integer> neg = new ArrayList<>();
 
@@ -18,12 +18,31 @@ public class AlternateNumbers {
             arr[2 * i] = pos.get(i);
             arr[2 * i + 1] = neg.get(i);
         }
-        return arr;
+    }
+
+    static int[] alternateNoBetter(int[] arr) {
+        int n = arr.length;
+        int posIndex = 0;
+        int negIndex = 1;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] < 0) {
+                ans[negIndex] = arr[i];
+                negIndex += 2;
+            } else {
+                ans[posIndex] = arr[i];
+                posIndex += 2;
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
-        int arr[] = {1, -3, 4, 5, -2, 6, -7, -4};
+        int[] arr = {1, -3, 4, 5, -2, 6, -7, -4};
         alternateNo(arr);
         Arrays.stream(arr).forEach(System.out::println);
+        int[] ans = alternateNoBetter(arr);
+        Arrays.stream(ans).forEach(System.out::println);
     }
 }
