0e0a0b36fbee1b5ec42956e6a24a9128ae411551
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-08 22:50:05 +0530

    Add alternateNoOptimal method to AlternateNumbers class for optimal rearrangement of uneven positive and negative elements

--- src/arrays/AlternateNumbers.java
+++ src/arrays/AlternateNumbers.java
@@ -38,13 +38,48 @@ public class AlternateNumbers {
         return ans;
     }
 
+    static void alternateNoOptimal(int[] arr) { // if an array contains uneven positive and negative elements
+        int n = arr.length;
+        List<Integer> pos = new ArrayList<>(), neg = new ArrayList<>();
 
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > 0)
+                pos.add(arr[i]);
+            else
+                neg.add(arr[i]);
+        }
+        if (pos.size() > neg.size()) {
+            for (int i = 0; i < neg.size(); i++) {
+                arr[2 * i] = pos.get(i);
+                arr[2 * i + 1] = neg.get(i);
+            }
+            int index = 2 * neg.size();
+            for (int i = neg.size(); i < pos.size(); i++) {
+                arr[index] = pos.get(i);
+                index++;
+            }
+        } else {
+            for (int i = 0; i < pos.size(); i++) {
+                arr[2 * i] = pos.get(i);
+                arr[2 * i + 1] = neg.get(i);
+            }
+            int index = 2 * pos.size();
+            for (int i = pos.size(); i < neg.size(); i++) {
+                arr[index] = neg.get(i);
+                index++;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         int[] arr = {1, -3, 4, 5, -2, 6, -7, -4};
         alternateNo(arr);
         Arrays.stream(arr).forEach(System.out::println);
+        System.out.println("--------------------------");
         int[] ans = alternateNoBetter(arr);
         Arrays.stream(ans).forEach(System.out::println);
+        System.out.println("--------------------------");
+        alternateNoOptimal(arr);
+        Arrays.stream(arr).forEach(System.out::println);
     }
 }
