48a3f9855c1eb4937b852b75001d62e26c01ce3b
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-09 23:05:53 +0530

    Add leaderOptimal method to LeaderInArray class for efficient leader finding

--- src/arrays/LeaderInArray.java
+++ src/arrays/LeaderInArray.java
@@ -1,6 +1,7 @@
 package arrays;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LeaderInArray {
@@ -21,9 +22,27 @@ public class LeaderInArray {
         return ans;
     }
 
+    static List<Integer> leaderOptimal(int[] arr) {
+        int n = arr.length;
+        int max = arr[n - 1];
+        List<Integer> ans = new ArrayList<>();
+        ans.add(arr[n - 1]);
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] > max) {
+                ans.add(arr[i]);
+                max = arr[i];
+            }
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] arr = {2, 5, 7, 3, 56, 4, 5, 6};
         List<Integer> ans = leader(arr);
         ans.forEach(System.out::println);
+        System.out.println("--------------");
+        List<Integer> ans2 = leaderOptimal(arr);
+        ans2.forEach(System.out::println);
     }
 }
