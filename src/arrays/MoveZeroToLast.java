b0c2f1f296e698ecb94e37a5603e958438e256c9
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-12 14:26:58 +0530

    Refactor loop in moveZero method to use enhanced for-loop for clarity

--- src/arrays/MoveZeroToLast.java
+++ src/arrays/MoveZeroToLast.java
@@ -8,9 +8,9 @@ public class MoveZeroToLast {
     static void moveZero(int[] arr) {
         int n = arr.length;
         List<Integer> temp = new ArrayList<>();
-        for (int i = 0; i < n; i++)
-            if (arr[i] != 0)
-                temp.add(arr[i]);
+        for (int j : arr)
+            if (j != 0)
+                temp.add(j);
         for (int i = 0; i < temp.size(); i++)
             arr[i] = temp.get(i);
         for (int i = temp.size(); i < n; i++)
