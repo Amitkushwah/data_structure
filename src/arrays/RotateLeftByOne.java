c6055dce19f621e8ddf3c30f3a35f0eb3ccd66c6
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-13 22:26:20 +0530

    Refactor rotate method in RotateLeftByOne class to eliminate return value and improve clarity

--- src/arrays/RotateLeftByOne.java
+++ src/arrays/RotateLeftByOne.java
@@ -3,13 +3,12 @@ package arrays;
 import java.util.Arrays;
 
 public class RotateLeftByOne {
-    static int[] rotate(int[] arr){
+    static void rotate(int[] arr){
         int temp = arr[0];
         for(int i = 1; i < arr.length; i++){
             arr[i - 1] = arr[i];
         }
         arr[arr.length-1] = temp;
-        return arr;
     }
 
     public static void main(String[] args) {
