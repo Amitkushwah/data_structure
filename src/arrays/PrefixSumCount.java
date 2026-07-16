0eb0a9dd75b61ef4d9baa8bb8a2aa7f577a406a0
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-16 22:15:43 +0530

    Add PrefixSumCount class to count subarrays with a given sum

---
+++ src/arrays/PrefixSumCount.java
@@ -0,0 +1,26 @@
+package arrays;
+
+public class PrefixSumCount {
+    static int prefixSumCount(int[] arr, int k) {
+        int n = arr.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int sum = 0;
+                for (int l = i; l <= j; l++)
+                    sum += arr[l];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, -3, 1, 1, 1, 4, 2, -3};
+        int k = 3;
+        System.out.println(prefixSumCount(arr, k));
+    }
+}
