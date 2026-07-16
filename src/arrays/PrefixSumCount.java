e90a1949aabd7520e0827437dda23b2d5ca151ff
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-16 22:50:12 +0530

    Add prefixSumCountOptimal method for efficient subarray sum counting

--- src/arrays/PrefixSumCount.java
+++ src/arrays/PrefixSumCount.java
@@ -1,5 +1,8 @@
 package arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PrefixSumCount {
     static int prefixSumCount(int[] arr, int k) {
         int n = arr.length;
@@ -33,10 +36,25 @@ public class PrefixSumCount {
         return count;
     }
 
+    static int prefixSumCountOptimal(int[] arr, int k) {
+        Map<Integer, Integer> preMap = new HashMap<>();
+        preMap.put(0, 1);
+        int sum = 0, count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+            int rem = sum - k;
+            count += preMap.getOrDefault(rem, 0);
+            preMap.put(sum, preMap.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, -3, 1, 1, 1, 4, 2, -3};
         int k = 3;
         System.out.println(prefixSumCount(arr, k));
         System.out.println(prefixSumCountBetter(arr, k));
+        System.out.println(prefixSumCountOptimal(arr, k));
     }
 }
