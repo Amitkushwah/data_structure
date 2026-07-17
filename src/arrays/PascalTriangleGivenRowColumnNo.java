b125f13a8e3055ff6ef2bcb339720c0dafa5d843
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-17 22:23:26 +0530

    Add PascalTriangleGivenRowColumnNo class to calculate binomial coefficients

---
+++ src/arrays/PascalTriangleGivenRowColumnNo.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class PascalTriangleGivenRowColumnNo {
+    static long nCR(int n, int r) {
+        long res = 1;
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int r = 7, c = 2;
+        System.out.println(nCR(r, c));
+    }
+}
