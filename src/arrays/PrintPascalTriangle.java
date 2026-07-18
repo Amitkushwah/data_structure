ea0e8c163550ca60bbacbc435675fd21bc7c3705
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-18 18:52:54 +0530

    Implement printPascalTriangleRow and printPascalTriangleOptimal methods for efficient row generation

--- src/arrays/PrintPascalTriangle.java
+++ src/arrays/PrintPascalTriangle.java
@@ -25,9 +25,32 @@ public class PrintPascalTriangle {
         return ans;
     }
 
+    static List<Long> printPascalTriangleRow(int n) {
+        long res = 1;
+        List<Long> ans = new ArrayList<>();
+        ans.add(res);
+        for (int c = 1; c < n; c++) {
+            res = res * (n - c);
+            res = res / c;
+            ans.add(res);
+        }
+        return ans;
+    }
+
+    static List<List<Long>> printPascalTriangleOptimal(int n) {
+        List<List<Long>> ansRow = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            ansRow.add(printPascalTriangleRow(i));
+        }
+        return ansRow;
+    }
+
     public static void main(String[] args) {
         int row = 5;
         List<List<Long>> ans = printPascalTriangleBruteForce(row);
         ans.forEach(System.out::println);
+        System.out.println("---------------");
+        List<List<Long>> ansOptimal = printPascalTriangleOptimal(row);
+        ansOptimal.forEach(System.out::println);
     }
 }
