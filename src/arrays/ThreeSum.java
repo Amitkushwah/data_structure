fb3a54a3838e5a6756a628af7e251bcd2f97f483
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-20 22:54:03 +0530

    Add ThreeSum class for finding unique triplets that sum to a target

---
+++ src/arrays/ThreeSum.java
@@ -0,0 +1,34 @@
+package arrays;
+
+import java.util.*;
+
+public class ThreeSum {
+    static List<List<Integer>> threeSum(int[] arr, int target) {
+        int n = arr.length;
+        List<List<Integer>> ans = new ArrayList<>();
+        Set<List<Integer>> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (arr[i] + arr[j] + arr[k] == target) {
+                        List<Integer> temp = new ArrayList<>();
+                        temp.add(arr[i]);
+                        temp.add(arr[j]);
+                        temp.add(arr[k]);
+                        Collections.sort(temp);
+                        set.add(temp);
+                    }
+                }
+            }
+        }
+        ans.addAll(set);
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = threeSum(arr, 0);
+        result.forEach(System.out::println);
+    }
+
+}
