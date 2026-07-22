3fef63aab2f11a750f6f5f8898e1ea211ca27ba8
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-22 21:41:46 +0530

    Add threeSumBetter method for improved triplet detection

--- src/arrays/ThreeSum.java
+++ src/arrays/ThreeSum.java
@@ -24,10 +24,36 @@ public class ThreeSum {
         return ans;
     }
 
+    static List<List<Integer>> threeSumBetter(int[] arr, int target) {
+        int n = arr.length;
+        Set<List<Integer>> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            Set<Integer> temp = new HashSet<>();
+            for (int j = i + 1; j < n; j++) {
+                int third = -(arr[i] + arr[j]);
+                if (temp.contains(third)) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(arr[i]);
+                    list.add(arr[j]);
+                    list.add(third);
+                    Collections.sort(list);
+                    set.add(list);
+                }
+                temp.add(arr[j]);
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>(set);
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] arr = {-1, 0, 1, 2, -1, -4};
         List<List<Integer>> result = threeSum(arr, 0);
         result.forEach(System.out::println);
+        System.out.println("--------------");
+        List<List<Integer>> result2 = threeSumBetter(arr, 0);
+        result2.forEach(System.out::println);
     }
 
 }
