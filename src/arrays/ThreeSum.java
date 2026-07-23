33de1e06c312217da0aa850cd7abaf4e757c4de4
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-23 21:27:23 +0530

    Add threeSumOptimal method for efficient triplet detection

--- src/arrays/ThreeSum.java
+++ src/arrays/ThreeSum.java
@@ -47,6 +47,38 @@ public class ThreeSum { // Given an array of N integers, your task is to find un
         return ans;
     }
 
+    static List<List<Integer>> threeSumOptimal(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+            int j = i + 1;
+            int k = n - 1;
+            while (j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if (sum < 0)
+                    j++;
+                else if (sum > 0)
+                    k--;
+                else {
+                    List<Integer> l = new ArrayList<>();
+                    l.add(arr[i]);
+                    l.add(arr[j]);
+                    l.add(arr[k]);
+                    ans.add(l);
+                    j++;
+                    k--;
+                    while (j < k && arr[j] == arr[j - 1])
+                        j++;
+                    while (j < k && arr[k] == arr[k - 1])
+                        k++;
+                }
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] arr = {-1, 0, 1, 2, -1, -4};
         List<List<Integer>> result = threeSum(arr);
@@ -54,6 +86,9 @@ public class ThreeSum { // Given an array of N integers, your task is to find un
         System.out.println("--------------");
         List<List<Integer>> result2 = threeSumBetter(arr);
         result2.forEach(System.out::println);
+        System.out.println("---------------");
+        List<List<Integer>> result3 = threeSumOptimal(arr);
+        result3.forEach(System.out::println);
     }
 
 }
