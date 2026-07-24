d50c371f5d81bf80ec46366dd7bc13fbebc151cb
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-24 21:25:35 +0530

    Add separator in main method output for better readability

--- src/arrays/MissingNumber.java
+++ src/arrays/MissingNumber.java
@@ -48,6 +48,7 @@ public class MissingNumber {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 5, 6};
         System.out.println(missingNo(arr));
+        System.out.println("--------------");
         System.out.println(missingNoBetter(arr));
         System.out.println(missingNoOptimal(arr));
     }
