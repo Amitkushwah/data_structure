fa0fa3ec7b3fc172cfe34d2e8d0cf24390b394d5
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-25 12:34:28 +0530

    Add SubarraySumZero class to find the length of the longest subarray with a sum of zero

---
+++ src/arrays/SubarraySumZero.java
@@ -0,0 +1,30 @@
+package arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumZero {
+    static int subarraySumZero(int[] arr) {
+        int n = arr.length;
+        int maxi = 0;
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            if (sum == 0)
+                maxi = i + 1;
+            if (map.containsKey(sum)) {
+                maxi = Math.max(maxi, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+        return maxi;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, -1, 3, 2, -2, -8, 1, 7, 10, 23};
+        System.out.println(subarraySumZero(arr));
+    }
+}
