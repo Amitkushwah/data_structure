233c1a717a22c23881cb0f10634db7aab0dabb26
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-28 21:36:44 +0530

    Add optimized method to find missing and repeating numbers in an array

--- src/arrays/MissingAndRepeatingNo.java
+++ src/arrays/MissingAndRepeatingNo.java
@@ -1,6 +1,8 @@
 package arrays;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class MissingAndRepeatingNo {
     static int[] missingAnsRepeatingNo(int[] arr, int n) {
@@ -21,10 +23,32 @@ public class MissingAndRepeatingNo {
         return new int[]{repeating, missing};
     }
 
+    static int[] missingAndRepeatingNoBetter(int[] arr, int n){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] hash = new int[n+1];
+        int repeating = -1;
+        int missing = -1;
+        for(int i = 0; i < n; i++){
+            hash[arr[i]]++;
+        }
+        for(int i = 1; i <= n; i++){
+            if(hash[i] == 2)
+                repeating = i;
+            if(hash[i] == 0)
+                missing = i;
+            if(repeating != -1 && missing != -1)
+                break;
+        }
+        return new int[]{repeating, missing};
+    }
+
     public static void main(String[] args) {
         int[] arr = {6, 3, 1, 4, 2, 1};
         int n = 6;
         int[] result = missingAnsRepeatingNo(arr, n);
         Arrays.stream(result).forEach(System.out::println);
+        System.out.println("---------------");
+        int[] resultBetter = missingAndRepeatingNoBetter(arr, n);
+        Arrays.stream(resultBetter).forEach(System.out::println);
     }
 }
