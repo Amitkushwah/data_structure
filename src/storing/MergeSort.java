a07abf6ee69639fa5aef28286366bc3053b44e4f
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-29 23:10:49 +0530

    Refactor formatting in MergeSort class for improved readability

--- src/storing/MergeSort.java
+++ src/storing/MergeSort.java
@@ -5,45 +5,46 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MergeSort {
-    static void divide(int[] arr, int low, int high){
-        if(low == high)
+    static void divide(int[] arr, int low, int high) {
+        if (low == high)
             return;
-        int mid = (low+high)/2;
+        int mid = (low + high) / 2;
         divide(arr, low, mid);
-        divide(arr, mid+1, high);
+        divide(arr, mid + 1, high);
         merge(arr, low, mid, high);
     }
-    static void merge(int[] arr, int low, int mid, int high){
+
+    static void merge(int[] arr, int low, int mid, int high) {
         List<Integer> temp = new ArrayList<>();
         int left = low;
-        int right = mid+1;
-        while(left <= mid && right <= high){
-            if(arr[left] <= arr[right]){
+        int right = mid + 1;
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
                 temp.add(arr[left]);
                 left++;
-            }
-            else {
+            } else {
                 temp.add(arr[right]);
                 right++;
             }
         }
-        while (left <= mid){
+        while (left <= mid) {
             temp.add(arr[left]);
             left++;
         }
-        while (right <= high){
+        while (right <= high) {
             temp.add(arr[right]);
             right++;
         }
         for (int i = low; i <= high; i++)
-                arr[i] = temp.get(i - low);
+            arr[i] = temp.get(i - low);
     }
-    static void mergeSort(int[] arr){
-        divide(arr, 0, arr.length-1);
+
+    static void mergeSort(int[] arr) {
+        divide(arr, 0, arr.length - 1);
     }
 
     public static void main(String[] args) {
-        int[] arr = {4,65,7,34,67,9,8,12,3};
+        int[] arr = {4, 65, 7, 34, 67, 9, 8, 12, 3};
         mergeSort(arr);
         Arrays.stream(arr).forEach(System.out::println);
     }
