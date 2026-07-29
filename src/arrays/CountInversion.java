3bee7a9d6d6ba7ab839b2c55a2f5878c643642ce
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-29 22:36:54 +0530

    Refactor formatting in CountInversion class for improved readability

--- src/arrays/CountInversion.java
+++ src/arrays/CountInversion.java
@@ -1,12 +1,12 @@
 package arrays;
 
 public class CountInversion {
-    static int countInversion(int[] arr){
+    static int countInversion(int[] arr) {
         int n = arr.length;
         int count = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                if(arr[i] > arr[j])
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j])
                     count++;
             }
         }
