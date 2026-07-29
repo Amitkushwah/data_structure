2fccf41f10869979bc005c61e62dfd879dfbeb1d
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-29 23:10:28 +0530

    Implement merge sort algorithm in CountInversion class for counting inversions

--- src/arrays/CountInversion.java
+++ src/arrays/CountInversion.java
@@ -1,5 +1,9 @@
 package arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CountInversion {
     static int countInversion(int[] arr) {
         int n = arr.length;
@@ -13,8 +17,59 @@ public class CountInversion {
         return count;
     }
 
+    static int divide(int[] arr, int low, int high){
+        int count = 0;
+        if(low == high)
+            return count;
+        int mid = (low + high) / 2;
+        count += divide(arr, low, mid);
+        count += divide(arr, mid + 1, high);
+        count += merge(arr, low, mid, high);
+        return count;
+    }
+
+    static int merge(int[] arr, int low, int mid, int high){
+        int left = low;
+        int right = mid + 1;
+        List<Integer> temp = new ArrayList<>();
+        int count = 0;
+
+        while(left <= mid && right <= high){
+            if(arr[left] <= arr[right]){
+                temp.add(arr[left]);
+                left++;
+            }
+            else {
+                temp.add(arr[right]);
+                count += (mid - left + 1);
+                right++;
+            }
+        }
+
+        while(left <= mid){
+            temp.add(arr[left]);
+            left++;
+        }
+        while (right <= high){
+            temp.add(arr[right]);
+            right++;
+        }
+
+        for(int i = low; i <= high; i++){
+            arr[i] = temp.get(i - low);
+        }
+
+        return count;
+    }
+
+    static int mergeSort(int[] arr){
+        return divide(arr, 0, arr.length - 1);
+    }
+
     public static void main(String[] args) {
         int[] arr = {5, 3, 2, 4, 1};
         System.out.println(countInversion(arr));
+        System.out.println("----------------");
+        System.out.println(mergeSort(arr));
     }
 }
