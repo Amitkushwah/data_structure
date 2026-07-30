b34afd797da9904aa7aaba74ae8ce62397d4a965
Amit Kushwah <kushwah2506@yahoo.com>
2026-07-30 23:10:55 +0530

    Refactor spacing and formatting in ReversePairs class for consistency

--- src/arrays/ReversePairs.java
+++ src/arrays/ReversePairs.java
@@ -4,49 +4,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReversePairs {
-    static int divide(int[] arr, int low, int high){
+    static int divide(int[] arr, int low, int high) {
         int mid = (low + high) / 2;
         int count = 0;
-        if(low == high) return count;
+        if (low == high) return count;
         count += divide(arr, low, mid);
         count += divide(arr, mid + 1, high);
         count += countPairs(arr, low, mid, high);
         merge(arr, low, mid, high);
         return count;
     }
-    static void merge(int[] arr, int low, int mid, int high){
+
+    static void merge(int[] arr, int low, int mid, int high) {
         int left = low;
         int right = mid + 1;
         List<Integer> temp = new ArrayList<>();
 
-        while(left <= mid && right <= high){
-            if(arr[left] < arr[right]){
+        while (left <= mid && right <= high) {
+            if (arr[left] < arr[right]) {
                 temp.add(arr[left]);
                 left++;
-            }
-            else {
+            } else {
                 temp.add(arr[right]);
                 right++;
             }
         }
-        while (left <= mid){
+        while (left <= mid) {
             temp.add(arr[left]);
             left++;
         }
-        while (right <= high){
+        while (right <= high) {
             temp.add(arr[right]);
             right++;
         }
-        for(int i = low; i <= high; i++){
+        for (int i = low; i <= high; i++) {
             arr[i] = temp.get(i - low);
         }
     }
 
-    static int countPairs(int[] arr, int low, int mid, int high){
+    static int countPairs(int[] arr, int low, int mid, int high) {
         int right = mid + 1;
         int count = 0;
-        for(int i = low; i <= mid; i++){
-            while (right <= high && arr[i] > 2 * arr[right]){
+        for (int i = low; i <= mid; i++) {
+            while (right <= high && arr[i] > 2 * arr[right]) {
                 right++;
             }
             count += (right - (mid + 1));
