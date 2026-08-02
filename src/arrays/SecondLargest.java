dba1397d8e38b37f73ac29a4bcad57940e4bd001
Amit Kushwah <kushwah2506@yahoo.com>
2026-08-02 21:14:00 +0530

    Refactor spacing and formatting in SecondLargest class for consistency

--- src/arrays/SecondLargest.java
+++ src/arrays/SecondLargest.java
@@ -1,32 +1,33 @@
 package arrays;
 
 public class SecondLargest {
-    static int secondLargestBetter(int[] arr){
+    static int secondLargestBetter(int[] arr) {
         int n = arr.length;
         int firstLargest = Integer.MIN_VALUE;
         int secondLargest = Integer.MIN_VALUE;
-        for(int i = 0; i < n; i++)
-            if(arr[i] > firstLargest)
+        for (int i = 0; i < n; i++)
+            if (arr[i] > firstLargest)
                 firstLargest = arr[i];
 
-        for(int i = 0; i < n; i++)
-            if(arr[i] > secondLargest && arr[i] != firstLargest)
+        for (int i = 0; i < n; i++)
+            if (arr[i] > secondLargest && arr[i] != firstLargest)
                 secondLargest = arr[i];
 
         return secondLargest;
 
     }
-    static int secondLargestOptimal(int[] arr){
+
+    static int secondLargestOptimal(int[] arr) {
         int n = arr.length;
         int firstLargest = Integer.MIN_VALUE;
         int secondLargest = Integer.MIN_VALUE;
 
-        for(int i = 0; i < n; i++){
-            if(arr[i] > firstLargest){
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > firstLargest) {
                 secondLargest = firstLargest;
                 firstLargest = arr[i];
             }
-            if(arr[i] < firstLargest && arr[i] > secondLargest){
+            if (arr[i] < firstLargest && arr[i] > secondLargest) {
                 secondLargest = arr[i];
             }
         }
@@ -34,7 +35,7 @@ public class SecondLargest {
     }
 
     public static void main(String[] args) {
-        int[] arr = {3,5,2,8,7,6,8,4};
+        int[] arr = {3, 5, 2, 8, 7, 6, 8, 4};
         System.out.println(secondLargestOptimal(arr));
         System.out.println(secondLargestBetter(arr));
     }
