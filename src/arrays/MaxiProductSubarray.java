df0c6fa6893f3928d7e2b8fc1a1c3e4760060051
Amit Kushwah <kushwah2506@yahoo.com>
2026-08-03 22:59:03 +0530

    Add optimal solution for maximum product of subarray in MaxiProductSubarray class

--- src/arrays/MaxiProductSubarray.java
+++ src/arrays/MaxiProductSubarray.java
@@ -14,8 +14,27 @@ public class MaxiProductSubarray {
         return ans;
     }
 
+    static int maxiProductSubarrayOptimal1(int[] arr) {
+        int n = arr.length;
+        int ans = Integer.MIN_VALUE;
+        int pre = 1;
+        int suf = 1;
+        for (int i = 0; i < n; i++) {
+            if (pre == 0)
+                pre = 1;
+            if (suf == 0)
+                suf = 1;
+            pre *= arr[i];
+            suf *= arr[n - i - 1];
+            ans = Math.max(ans, Math.max(pre, suf));
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int[] arr = {2, 3, -2, 4};
         System.out.println(maxiProductSubarray(arr));
+        System.out.println("------------------");
+        System.out.println(maxiProductSubarrayOptimal1(arr));
     }
 }
