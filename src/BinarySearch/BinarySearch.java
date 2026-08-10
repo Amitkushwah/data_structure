4f1421cded729560b46a3c2f7ab766a5ecad914e
Amit Kushwah <kushwah2506@yahoo.com>
2026-08-10 23:03:10 +0530

    Format code in BinarySearch for improved readability

--- src/BinarySearch/BinarySearch.java
+++ src/BinarySearch/BinarySearch.java
@@ -1,13 +1,13 @@
 package BinarySearch;
 
 public class BinarySearch {
-    static int search(int[] arr, int target){
+    static int search(int[] arr, int target) {
         int low = 0, high = arr.length - 1;
-        while(low <= high){
+        while (low <= high) {
             int mid = (low + high) / 2;
-            if(arr[mid] == target)
+            if (arr[mid] == target)
                 return mid;
-            else if(target > arr[mid])
+            else if (target > arr[mid])
                 low = mid + 1;
             else high = mid - 1;
         }
@@ -15,7 +15,7 @@ public class BinarySearch {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,3,5,6,8,9};
+        int[] arr = {1, 3, 5, 6, 8, 9};
         System.out.println(search(arr, 9));
     }
 }
