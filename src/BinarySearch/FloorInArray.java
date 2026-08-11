5f2f17ffc54fb2316c8581169974e3897640467c
Amit Kushwah <kushwah2506@yahoo.com>
2026-08-11 22:53:41 +0530

    Add FloorInArray class with floorInArray method for finding the floor value in a sorted array

---
+++ src/BinarySearch/FloorInArray.java
@@ -0,0 +1,23 @@
+package BinarySearch;
+
+public class FloorInArray {
+    static int floorInArray(int[] arr, int target){
+        int n = arr.length;
+        int low = 0, high = n -1;
+        int ans = -1;
+        while(low <= high){
+            int mid  = (low + high) / 2;
+            if(arr[mid] <= target){
+                ans = arr[mid];
+                low = mid + 1;
+            }
+            else high = mid - 1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 3, 5, 8, 8, 10, 10, 11};
+        System.out.println(floorInArray(arr, 4));
+    }
+}
