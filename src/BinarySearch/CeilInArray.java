ad2fae3f8a1848871d4f38d62986f04916ac075c
Amit Kushwah <kushwah2506@yahoo.com>
2026-08-11 22:57:51 +0530

    Add CeilInArray class with ceilInArray method for finding the ceiling value in a sorted array

---
+++ src/BinarySearch/CeilInArray.java
@@ -0,0 +1,22 @@
+package BinarySearch;
+
+public class CeilInArray {
+    static int ceilInArray(int[] arr, int target) {
+        int n = arr.length;
+        int low = 0, high = n - 1;
+        int ans = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= target) {
+                ans = arr[mid];
+                high = mid - 1;
+            } else low = mid + 1;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 3, 5, 8, 8, 10, 10, 11};
+        System.out.println(ceilInArray(arr, 4));
+    }
+}
