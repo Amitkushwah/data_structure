4cb0a1168ade3b1627576234c0c646a7776b06fb
Amit Kushwah <kushwah2506@yahoo.com>
2026-08-11 21:41:55 +0530

    Format searchLowerBond method and main array initialization for improved readability

--- src/BinarySearch/LowerBond.java
+++ src/BinarySearch/LowerBond.java
@@ -1,18 +1,17 @@
 package BinarySearch;
 
 public class LowerBond {
-    static int searchLowerBond(int[] arr, int target){
+    static int searchLowerBond(int[] arr, int target) {
         int n = arr.length;
         int low = 0;
         int high = n - 1;
         int ans = n;
-        while(low <= high){
-            int mid = (low + high)/2;
-            if(arr[mid] >= target){
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= target) {
                 ans = mid;
                 high = mid - 1;
-            }
-            else{
+            } else {
                 low = mid + 1;
             }
         }
@@ -20,7 +19,7 @@ public class LowerBond {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,3,5,8,8,10,10,11};
+        int[] arr = {1, 2, 3, 3, 5, 8, 8, 10, 10, 11};
         System.out.println(searchLowerBond(arr, 4));
     }
 }
