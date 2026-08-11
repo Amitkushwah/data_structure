aff6bb9bf72d471f303440617629efc688eea914
Amit Kushwah <kushwah2506@yahoo.com>
2026-08-11 22:53:48 +0530

    Refactor floorInArray method for improved readability and consistency

--- src/BinarySearch/FloorInArray.java
+++ src/BinarySearch/FloorInArray.java
@@ -1,17 +1,16 @@
 package BinarySearch;
 
 public class FloorInArray {
-    static int floorInArray(int[] arr, int target){
+    static int floorInArray(int[] arr, int target) {
         int n = arr.length;
-        int low = 0, high = n -1;
+        int low = 0, high = n - 1;
         int ans = -1;
-        while(low <= high){
-            int mid  = (low + high) / 2;
-            if(arr[mid] <= target){
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] <= target) {
                 ans = arr[mid];
                 low = mid + 1;
-            }
-            else high = mid - 1;
+            } else high = mid - 1;
         }
         return ans;
     }
