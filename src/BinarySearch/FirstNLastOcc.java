da2c5e9680d2703557e283999917480b10ddecb7
Amit Kushwah <kushwah2506@yahoo.com>
2026-08-12 22:08:01 +0530

    Format findOcc method for improved readability and consistency

--- src/BinarySearch/FirstNLastOcc.java
+++ src/BinarySearch/FirstNLastOcc.java
@@ -3,13 +3,13 @@ package BinarySearch;
 import java.util.Arrays;
 
 public class FirstNLastOcc {
-    static int[] findOcc(int[] arr, int target){
+    static int[] findOcc(int[] arr, int target) {
         int n = arr.length;
         int first = -1;
         int last = -1;
-        for(int i = 0; i < n; i++){
-            if(arr[i] == target){
-                if(first == -1)
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == target) {
+                if (first == -1)
                     first = i;
                 last = i;
             }
@@ -18,7 +18,7 @@ public class FirstNLastOcc {
     }
 
     public static void main(String[] args) {
-        int[] arr = {2,4,6,8,8,8,11,13};
+        int[] arr = {2, 4, 6, 8, 8, 8, 11, 13};
         Arrays.stream(findOcc(arr, 8)).forEach(System.out::println);
     }
 }
