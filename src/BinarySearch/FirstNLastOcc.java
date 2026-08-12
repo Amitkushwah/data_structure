700a2d3b8d606fccc1943ae7003a4d9841155f6b
Amit Kushwah <kushwah2506@yahoo.com>
2026-08-12 22:57:35 +0530

    Add findFirst and findLast methods for binary search of target occurrences

--- src/BinarySearch/FirstNLastOcc.java
+++ src/BinarySearch/FirstNLastOcc.java
@@ -60,10 +60,58 @@ public class FirstNLastOcc {
         return new int[]{lb, ub - 1};
     }
 
+    static int findFirst(int[] arr, int target){
+        int n = arr.length;
+        int low = 0;
+        int high = n - 1;
+        int ans = -1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[mid] == target){
+                ans = mid;
+                high = mid - 1;
+            }
+            else if(arr[mid] < target){
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    static int findLast(int[] arr, int target){
+        int n = arr.length;
+        int low = 0;
+        int high = n - 1;
+        int ans = -1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[mid] == target){
+                ans = mid;
+                low = mid + 1;
+            }
+            else if(arr[mid] < target){
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    static int[] findOccWithBinarySearch(int[] arr, int target){
+        return new int[]{findFirst(arr, target), findLast(arr, target)};
+    }
+
     public static void main(String[] args) {
         int[] arr = {2, 4, 6, 8, 8, 8, 11, 13};
         Arrays.stream(findOcc(arr, 8)).forEach(System.out::println);
         System.out.println("----------------");
         Arrays.stream(findOccOptimal(arr, 8)).forEach(System.out::println);
+        System.out.println("----------------");
+        Arrays.stream(findOccWithBinarySearch(arr, 8)).forEach(System.out::println);
     }
 }
