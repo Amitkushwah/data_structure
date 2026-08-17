8e8cda073aa3ce02355e40bf863703976e25b7f3
Amit Kushwah <kushwah2506@yahoo.com>
2026-08-17 22:13:06 +0530

    add more elements in an array

--- src/BinarySearch/SearchInRotatedArray.java
+++ src/BinarySearch/SearchInRotatedArray.java
@@ -27,7 +27,7 @@ public class SearchInRotatedArray {
     }
 
     public static void main(String[] args) {
-        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
         int target = 2;
         System.out.println(search(arr, target));
     }
