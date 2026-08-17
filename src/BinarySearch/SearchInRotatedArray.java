793ad02a88ae4bd52252b34c07b35fec0aed18b6
Amit Kushwah <kushwah2506@yahoo.com>
2026-08-17 22:01:35 +0530

    add SearchInRotatedArray class to search an element in rotated array

---
+++ src/BinarySearch/SearchInRotatedArray.java
@@ -0,0 +1,38 @@
+package BinarySearch;
+
+public class SearchInRotatedArray {
+    static int search(int[] arr, int target){
+        int n = arr.length;
+        int low = 0;
+        int high = n - 1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[low] <= arr[mid]){
+                if(target >= arr[low] && target <= arr[mid]){
+                    high = mid - 1;
+                }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else {
+                if(target >= arr[mid] && target <= high){
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {4,5,6,7,0,1,2};
+        int target = 2;
+        System.out.println(search(arr, target));
+    }
+}
