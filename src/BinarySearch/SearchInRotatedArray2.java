aa5be8851ed6b4bad30ce46b519b7e09c7748006
Amit Kushwah <kushwah2506@yahoo.com>
2026-08-18 21:42:52 +0530

    format code in SearchInRotatedArray2 for improved readability

--- src/BinarySearch/SearchInRotatedArray2.java
+++ src/BinarySearch/SearchInRotatedArray2.java
@@ -1,33 +1,29 @@
 package BinarySearch;
 
 public class SearchInRotatedArray2 {
-    static boolean search(int[] arr, int target){
+    static boolean search(int[] arr, int target) {
         int n = arr.length;
         int low = 0;
         int high = n - 1;
 
-        while(low <= high){
+        while (low <= high) {
             int mid = (low + high) / 2;
-            if(arr[mid] == target){
+            if (arr[mid] == target) {
                 return true;
             }
-            if(arr[mid] == arr[low] && arr[mid] == arr[high]){
+            if (arr[mid] == arr[low] && arr[mid] == arr[high]) {
                 low = low + 1;
                 high = high - 1;
-            }
-            else if(arr[low] <= arr[mid]){
-                if(target >= arr[low] && target <= arr[mid]){
+            } else if (arr[low] <= arr[mid]) {
+                if (target >= arr[low] && target <= arr[mid]) {
                     high = mid - 1;
-                }
-                else {
+                } else {
                     low = mid + 1;
                 }
-            }
-            else {
-                if(target >= arr[mid] && target <= arr[high]){
+            } else {
+                if (target >= arr[mid] && target <= arr[high]) {
                     low = mid + 1;
-                }
-                else {
+                } else {
                     high = mid - 1;
                 }
             }
@@ -36,7 +32,7 @@ public class SearchInRotatedArray2 {
     }
 
     public static void main(String[] args) {
-        int[] arr = {3,1,2,3,3,3,3};
+        int[] arr = {3, 1, 2, 3, 3, 3, 3};
         int target = 1;
         System.out.println(search(arr, target));
     }
