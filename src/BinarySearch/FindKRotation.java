edfcbdde2a6a4eeba345ab687db2295d46335b4a
Amit Kushwah <kushwah2506@yahoo.com>
2026-08-18 22:58:55 +0530

    format code in FindKRotation for improved readability

--- src/BinarySearch/FindKRotation.java
+++ src/BinarySearch/FindKRotation.java
@@ -1,34 +1,33 @@
 package BinarySearch;
 
 public class FindKRotation {
-    static int findKRotation(int[] arr){
+    static int findKRotation(int[] arr) {
         int n = arr.length;
         int low = 0, high = n - 1;
         int mini = Integer.MAX_VALUE;
         int index = -1;
         while (low <= high) {
             int mid = (low + high) / 2;
-            if(arr[low] == arr[mid] && arr[mid] == arr[high]){
+            if (arr[low] == arr[mid] && arr[mid] == arr[high]) {
                 low++;
                 high--;
                 continue;
             }
-            if(arr[low] <= arr[high]){
-                if(arr[low] < mini){
+            if (arr[low] <= arr[high]) {
+                if (arr[low] < mini) {
                     mini = arr[low];
                     index = low;
                 }
                 break;
             }
-            if(arr[low] <= arr[mid]){
-                if(arr[low] < mini){
+            if (arr[low] <= arr[mid]) {
+                if (arr[low] < mini) {
                     mini = arr[low];
                     index = low;
                 }
                 low = mid + 1;
-            }
-            else{
-                if(arr[mid] < mini){
+            } else {
+                if (arr[mid] < mini) {
                     mini = arr[mid];
                     index = mid;
                 }
@@ -39,7 +38,7 @@ public class FindKRotation {
     }
 
     public static void main(String[] args) {
-        int[] arr = {3,4, 5, 6, 1, 2};
+        int[] arr = {3, 4, 5, 6, 1, 2};
         System.out.println(findKRotation(arr));
     }
 }
