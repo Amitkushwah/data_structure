818c170612e37abda5867e426c9fcdf700dfac4c
Amit Kushwah <kushwah2506@yahoo.com>
2026-08-18 22:58:31 +0530

    add FindKRotation class to determine the number of rotations in a rotated sorted array

---
+++ src/BinarySearch/FindKRotation.java
@@ -0,0 +1,45 @@
+package BinarySearch;
+
+public class FindKRotation {
+    static int findKRotation(int[] arr){
+        int n = arr.length;
+        int low = 0, high = n - 1;
+        int mini = Integer.MAX_VALUE;
+        int index = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if(arr[low] == arr[mid] && arr[mid] == arr[high]){
+                low++;
+                high--;
+                continue;
+            }
+            if(arr[low] <= arr[high]){
+                if(arr[low] < mini){
+                    mini = arr[low];
+                    index = low;
+                }
+                break;
+            }
+            if(arr[low] <= arr[mid]){
+                if(arr[low] < mini){
+                    mini = arr[low];
+                    index = low;
+                }
+                low = mid + 1;
+            }
+            else{
+                if(arr[mid] < mini){
+                    mini = arr[mid];
+                    index = mid;
+                }
+                high = mid - 1;
+            }
+        }
+        return index;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,4, 5, 6, 1, 2};
+        System.out.println(findKRotation(arr));
+    }
+}
