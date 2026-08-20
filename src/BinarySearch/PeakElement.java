29e4f7fa6c909013dcbefdc3ffad1aa433c2a354
Amit Kushwah <kushwah2506@yahoo.com>
2026-08-20 21:24:05 +0530

    format code in PeakElement for improved readability

--- src/BinarySearch/PeakElement.java
+++ src/BinarySearch/PeakElement.java
@@ -1,10 +1,10 @@
 package BinarySearch;
 
 public class PeakElement {
-    static int findPeakElement(int[] arr){
+    static int findPeakElement(int[] arr) {
         int n = arr.length;
-        for(int i = 0; i < n; i++){
-            if((i == 0 || arr[i] > arr[i - 1]) && (i == n-1 || arr[i] > arr[i+1])){
+        for (int i = 0; i < n; i++) {
+            if ((i == 0 || arr[i] > arr[i - 1]) && (i == n - 1 || arr[i] > arr[i + 1])) {
                 return i;  // return index of the Peak
             }
         }
@@ -12,7 +12,7 @@ public class PeakElement {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,3,2,1};
+        int[] arr = {1, 2, 3, 4, 5, 6, 3, 2, 1};
         System.out.println(findPeakElement(arr));
     }
 }
