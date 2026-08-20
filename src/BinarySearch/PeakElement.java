d8af355fd026b5b231036730275cd6082bbb2025
Amit Kushwah <kushwah2506@yahoo.com>
2026-08-20 21:23:53 +0530

    add PeakElement class to find the peak element in an array

---
+++ src/BinarySearch/PeakElement.java
@@ -0,0 +1,18 @@
+package BinarySearch;
+
+public class PeakElement {
+    static int findPeakElement(int[] arr){
+        int n = arr.length;
+        for(int i = 0; i < n; i++){
+            if((i == 0 || arr[i] > arr[i - 1]) && (i == n-1 || arr[i] > arr[i+1])){
+                return i;  // return index of the Peak
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,3,2,1};
+        System.out.println(findPeakElement(arr));
+    }
+}
