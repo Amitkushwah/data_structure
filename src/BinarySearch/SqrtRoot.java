cad1e1862bd2095d88b8b83f84c40be756501fd7
Amit Kushwah <kushwah2506@yahoo.com>
2026-09-10 22:24:23 +0530

    feat: implement square root calculation using linear search

---
+++ src/BinarySearch/SqrtRoot.java
@@ -0,0 +1,19 @@
+package BinarySearch;
+
+public class SqrtRoot {
+    static int sqrtRoot(int n){
+        int ans = -1;
+        for(int i = 1; i <= n; i++){
+            if(i * i <= n )
+                ans = i;
+            else
+                break;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int n = 37;
+        System.out.println(sqrtRoot(n));
+    }
+}
